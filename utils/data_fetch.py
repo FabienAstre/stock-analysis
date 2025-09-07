b1fe0351651014d8ff6c12d72cef88e15fb4fcb6
FabienAstre <astrefabien@gmail.com>
2025-09-07 12:13:27 -0700

    Delete utils directory

--- utils/data_fetch.py
+++
@@ -1,8 +0,0 @@
-import yfinance as yf
-import pandas as pd
-import numpy as np
-
-def fetch_price_data(ticker, period="1y"):
-    """Fetch historical OHLC data from Yahoo Finance."""
-    data = yf.download(ticker, period=period)
-    return data
